c02c74105ed94ecf100989bc843d42757f92275a
Tesla <jeyeshsec@gmail.com>
2024-02-17 20:33:46 +0530

    front-end

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, request, render_template, jsonify
+import requests
+import base64
+from bs4 import BeautifulSoup
+import urllib.parse
+from urllib.parse import urljoin
+import json
+import html
+app = Flask(__name__)
+
+API_URL = "http://127.0.0.1:6969/api/analyze-url"
+
+@app.route('/',  methods=['GET','POST'])
+def home():
+    try:
+        if request.method == 'POST' and 'url' in request.form:
+            url = request.form['url'].strip()
+            # Encode URL to base64
+            encoded_url = base64.urlsafe_b64encode(url.encode('utf-8')).decode('utf-8')
+            # JSON data containing the encoded URL
+            data = {'url': encoded_url}
+            # Send POST request to the API endpoint
+            response = requests.post(API_URL, json=data)
+            if response.status_code == 200:
+                # Convert JSON response to dictionary
+                output = response.json()
+            else:
+                output = {'status': 'ERROR', 'msg': f"Failed to analyze URL: {response.status_code} - {response.text}"}
+        else:
+            output = 'NA'
+    except Exception as e:
+        output = {'status': 'ERROR', 'msg': str(e)}
+
+    return render_template('index.html', output=output)
+
+
+@app.route('/preview', methods=['POST'])
+def preview():
+    try:
+        url = request.form.get('url')
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # inject external resources into HTML
+        for link in soup.find_all('link'):
+            if link.get('href'):
+                link['href'] = urljoin(url, link['href'])
+        
+        # Uncomment this if you want to enable script
+        # for script in soup.find_all('script'):
+        #     if script.get('src'):
+        #         script['src'] = urljoin(url, script['src'])
+
+        for img in soup.find_all('img'):
+            if img.get('src'):
+                img['src'] = urljoin(url, img['src'])
+
+        return render_template('preview.html', content=soup.prettify())
+    except Exception as e:
+        return  f"Error: {e}"
+
+
+@app.route('/source-code', methods=['GET','POST'])
+def view_source_code():
+
+    try:
+        url = request.form.get('url')
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        formatted_html = soup.prettify()
+        
+        return render_template('source_code.html', formatted_html = formatted_html, url = url)
+    
+    except Exception as e:
+        return  f"Error: {e}"
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=True)
